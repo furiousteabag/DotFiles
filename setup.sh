ff9e87c9e48f00fefbcfd4e00c373b0ac062b5be
Alexander Smirnov <furiousteabag@gmail.com>
2022-07-20 11:37:41 +0200

    correct symlinks

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-cp -rs $PWD/.config/ ~/.config/
-cp -rs $PWD/.local/ ~/.local/
+cp -rs $PWD/.config/ ~/
+cp -rs $PWD/.local/ ~/
 cp -rs $PWD/.zprofile ~/
+cp -rs $PWD/.xinitrc ~/
 chsh -s $(which zsh)
 git clone https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.local/share/zsh/plugins/zsh-syntax-highlighting/
 git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.local/share/zsh/plugins/zsh-autosuggestions/
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
-echo "default_linemode devicons" >> $HOME/.config/ranger/rc.conf
+#echo "default_linemode devicons" >> $HOME/.config/ranger/rc.conf
 git clone https://github.com/tmux-plugins/tpm ~/.local/share/tmux/plugins/tpm
